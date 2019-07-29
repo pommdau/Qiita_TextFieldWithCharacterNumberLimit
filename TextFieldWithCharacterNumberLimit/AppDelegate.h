4a1829797aa0ccc5499a781567207beaf272325f
hikeuchi <hikeuchi@morisawa.co.jp>
2019-07-29 11:05:08 +0900

    Initial Commit

---
+++ TextFieldWithCharacterNumberLimit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextFieldWithCharacterNumberLimit
+//
+//  Created by HIROKI IKEUCHI on 2019/07/29.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TextFieldWithCharacterNumberLimit/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TextFieldWithCharacterNumberLimit
+//
+//  Created by HIROKI IKEUCHI on 2019/07/29.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
